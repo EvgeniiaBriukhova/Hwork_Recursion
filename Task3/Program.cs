3f5350a9cc98135fec33b841668bdb36c6a91ee2
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-06-04 13:52:45 +0600

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 3: Задайте произвольный массив. Выведете его элементы,
+начиная с конца. Использовать рекурсию, не использовать циклы. */
+
+void PrintArrayReversed(int[] array, int i)
+{
+    if( i >= 0)
+    {
+        Console.Write(array[i] + " ");
+        PrintArrayReversed(array, i - 1);
+    }
+}
+
+
+int[] array = {5,3,2,1};
+PrintArrayReversed(array, array.Length - 1);
