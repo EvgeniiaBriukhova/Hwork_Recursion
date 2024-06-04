308c60e19e4b6c328ee921c9cd708404bf62c4ed
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-06-04 12:06:16 +0600

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 1: Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы. */
+
+void PrintRangeOfNumbersMN(int num1, int num2)
+    {
+        if (num1 < num2)
+        {
+            Console.Write($"{num1} ");
+            PrintRangeOfNumbersMN(num1 + 1, num2);
+        }
+        else if (num1 > num2)
+        {
+            Console.Write($"{num1} ");
+            PrintRangeOfNumbersMN(num1 - 1, num2);
+        }
+        else
+        {
+            Console.Write($"{num1} ");
+        }
+    }
+
+
+Console.WriteLine("Input first number for M: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number for N: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+PrintRangeOfNumbersMN(num1, num2);
