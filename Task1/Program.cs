9a95d2575e9f2befc857c1c0b02916e05898290e
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-06-05 13:59:18 +0600

    add changes 01 in Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,28 +2,36 @@
 которая выведет все натуральные числа в промежутке от M до N. 
 Использовать рекурсию, не использовать циклы. */
 
-void PrintRangeOfNumbersMN(int num1, int num2)
+static void PrintRangeOfNumbersMN(int num1, int num2)
+{
+    if (num1 < num2)
     {
-        if (num1 < num2)
-        {
-            Console.Write($"{num1} ");
-            PrintRangeOfNumbersMN(num1 + 1, num2);
-        }
-        else if (num1 > num2)
-        {
-            Console.Write($"{num1} ");
-            PrintRangeOfNumbersMN(num1 - 1, num2);
-        }
-        else
-        {
-            Console.Write($"{num1} ");
-        }
+        Console.Write($"{num1} ");
+        PrintRangeOfNumbersMN(num1 + 1, num2);
     }
+    else if (num1 > num2)
+    {
+        Console.Write($"{num1} ");
+        PrintRangeOfNumbersMN(num1 - 1, num2);
+    }
+    else
+    {
+        Console.Write($"{num1} ");
+    }
+}
 
 
-Console.WriteLine("Input first number for M: ");
+Console.WriteLine("Input first positive number for M: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input second number for N: ");
+Console.WriteLine("Input second positive number for N: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-PrintRangeOfNumbersMN(num1, num2);
+if (num1 < 0 || num2 < 0)
+{
+    Console.WriteLine("Input error");
+}
+else
+{
+    PrintRangeOfNumbersMN(num1, num2);
+}
+
